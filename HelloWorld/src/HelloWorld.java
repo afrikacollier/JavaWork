3e109e1a7cc19014381e27fadbbfd6cb70b38c58
afrikacollier <afrikakcfc@gmail.com>
2022-03-28 21:42:09 -0700

    Create HelloWorld.java
    HelloWorld.java file from Eclipse
    Create a small program that defines some fields. Try creating some illegal field names and see what kind of error the compiler produces. Use the naming rules and conventions as a guide. In the program you created, try leaving the fields uninitialized and print out their values. Try the same with a local variable and see what kind of compiler errors you can produce. Becoming familiar with common compiler errors will make it easier to recognize bugs in your code.

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,23 @@
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		/*
+		    1.Java source file name mismatch (class and source file mismatch)
+			2.Improper casing
+	        3.Mismatched brackets
+			4.Missing semicolons
+			5.Method is undefined
+			6.Variable already defined
+			7.Variable not initialized
+			8.Type mismatch: cannot convert
+			9.Return type required
+			10.Unreachable code
+		 */
+		//Improper casing
+		int x = 10;
+		System.out.println(x);
+		int y;
+		System.out.println(y);
+		
+	}
+}
